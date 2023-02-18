718f80e89515cc2579e94b9c4842702c48f1155c
arvydasg <azegaspa@gmail.com>
2023-02-18 12:29:52 +0200

    did one migration

--- web_site/models.py
+++ web_site/models.py
@@ -7,11 +7,13 @@ class Message(db.Model):
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     message = db.Column(db.Text, nullable=False)
+    city = db.Column(db.String(80), nullable=True)
 
     def __init__(self, name, email, message):
         self.name = name
         self.email = email
         self.message = message
+        self.city = city
 
     def __repr__(self):
-        return f"{self.name} - {self.email}"
+        return f"{self.name} - {self.email}, from {self.city}"
