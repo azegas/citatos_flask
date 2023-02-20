917c53253d154453f4e008ab5b8892465b180b8c
arvydasg <azegaspa@gmail.com>
2023-02-20 21:28:56 +0200

    added additional db info

--- web_site/populate.py
+++ web_site/populate.py
@@ -38,21 +38,29 @@ with app.app_context():
     bill_quote = Quote(
         text="Anything worth having in life is just getting through bombing",
         status="published",
+        date_created=1999,
+        score=100,
         author=bill,
     )
     nir_quote = Quote(
         text="Slaves to the urgent at the cost of the important",
         status="published",
+        date_created=1992,
+        score=44,
         author=nir,
     )
     unknown_quote = Quote(
         text="Beautiful things don't ask for attention",
         status="published",
+        date_created=2004,
+        score=1022,
         author=unknown,
     )
     silvanus_quote = Quote(
         text="What one fool can do, another can",
         status="published",
+        date_created=2344,
+        score=111,
         author=silvanus,
     )
     db.session.add_all([bill_quote, nir_quote, unknown_quote, silvanus_quote])
