402b695ceba049dcd30585d2535465a93477ce10
arvydasg <azegaspa@gmail.com>
2023-02-21 09:28:15 +0200

    added a comment

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from web_site.models import db
 
 if __name__ == "__main__":
     with app.app_context():  # kad db sesiju dalykai nesusipintu
-        db.create_all()
+        db.create_all()  # creates db if the db doesn't already exist
     app.run(debug=True)
